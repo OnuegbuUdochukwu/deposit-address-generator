8d0e82bdafcddbc65020098374c65c2912537cae
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-19 18:16:37 +0100

    Initial Commit

---
+++ src/main/java/com/codewithudo/depositaddressgenerator/DepositAddressGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.codewithudo.depositaddressgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositAddressGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositAddressGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithudo/depositaddressgenerator/DepositAddressGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithudo.depositaddressgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositAddressGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
